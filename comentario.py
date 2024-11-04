87af832fa172572ea36c2a5299ccb86d455d63b5
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-11-03 20:03:01 -0500

    feat: Add Bucket Ingest functionality

--- comentario.py
+++ comentario.py
@@ -1,6 +1,7 @@
 import boto3
 import uuid
 import os
+import json
 
 def lambda_handler(event, context):
     # Entrada (json)
@@ -8,6 +9,7 @@ def lambda_handler(event, context):
     tenant_id = event['body']['tenant_id']
     texto = event['body']['texto']
     nombre_tabla = os.environ["TABLE_NAME"]
+    bucket_name = os.environ["BUCKET_NAME"]
     # Proceso
     uuidv1 = str(uuid.uuid1())
     comentario = {
@@ -20,6 +22,14 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=comentario)
+
+    s3 = boto3.client('s3')
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=f'comentarios/{uuidv1}.json',
+        Body=json.dumps(comentario)
+    )
+    
     # Salida (json)
     print(comentario)
     return {
